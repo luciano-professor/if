ab893302330b98b5c18bc325c8b1b91b4235eee3
luciano-professor <luciano.souza@ifnmg.edu.br>
2020-03-03 22:25:21 -0300

    Começando a criar a pagina de Listar Curso

---
+++ resources/views/admin/cursos/listar.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+
+    <title>IF - Cursos</title>
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">IF - Admin</a>
+
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="" class="nav-link">Cursos</a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">Turmas</a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">Matrizes</a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">Alunos</a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">Matrículas</a>
+            </li>
+        </ul>
+
+    </nav>
+
+</body>
+</html>
